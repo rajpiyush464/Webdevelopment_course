7399d618089d8625636c007c343c920bc5fdf14d
Piyush <piyushraj5190@gmail.com>
2024-11-30 12:51:47 +0530

    Maths Basics

---
+++ math.js
@@ -0,0 +1,14 @@
+//// NUMBERS //
+
+let num=123.89453
+//console.log(num.toPrecision(5))
+let hundereds=100000000
+//console.log(hundereds.toLocaleString())
+//console.log(hundereds.toLocaleString('en-IN'))
+
+let a=345.56
+ 
+//console.log(Math.ceil(a))
+//console.log(Math.round(45.89))
+console.log(Math.random())
+console.log((Math.random()*10)+1)
