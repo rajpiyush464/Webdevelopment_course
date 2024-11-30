ecbd1e86abdec4c6e5d40d7d1115f3ccd3ea6963
Piyush <piyushraj5190@gmail.com>
2024-11-30 12:29:31 +0530

    Strings in JavaScript

---
+++ string.js
@@ -0,0 +1,20 @@
+// Learn Strings with me 
+   
+ let name="Piyush Raj"
+ let age=19
+ //console.log("starting string")
+  //console.log(`hellow my name is ${name} and my age is ${age}`)
+
+
+ let  a="       Piyush       "
+ //console.log(a.trim())    // to Earse the spaces
+ 
+ let end="Larsen and Turbo Technologies Services"
+ // some methods
+ //console.log(end.substring(2,5))
+ //console.log(end.length)
+ //console.log(end.toUpperCase())
+ const gen= new String("Rahul")
+ //console.log(gen.indexOf("R"))
+ console.log(gen.charAt(3))
+
